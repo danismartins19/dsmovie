33e3d2477e4d69921f46b901ddb0b985028f0d01
Daniel Martins <62864768+danismartins19@users.noreply.github.com>
2022-01-14 12:31:38 -0300

    classes implemented

---
+++ backend/src/main/java/com/danismartins19/dsmovie/entities/ScorePK.java
@@ -0,0 +1,27 @@
+package com.danismartins19.dsmovie.entities;
+
+public class ScorePK {
+	private Movie movie;
+	private User user;
+	
+	public ScorePK() {
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+}
