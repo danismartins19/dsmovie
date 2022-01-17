f05aebcb397718d9076af4fb5f12156e6b78c529
Daniel Martins <danidiamoo1@gmail.com>
2022-01-17 16:38:10 -0300

    Domain model, database seed

--- backend/src/main/java/com/danismartins19/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/danismartins19/dsmovie/entities/Movie.java
@@ -1,6 +1,17 @@
 package com.danismartins19.dsmovie.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
 public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private Double score;
--- backend/src/main/java/com/danismartins19/dsmovie/entities/User.java
+++ backend/src/main/java/com/danismartins19/dsmovie/entities/User.java
@@ -1,6 +1,17 @@
 package com.danismartins19.dsmovie.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
 	
